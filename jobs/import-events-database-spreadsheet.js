09bea04101429e4d695aa120124de737e64b4a7d
Leon Miller-Out <leon@singlebrook.com>
2019-09-04 23:33:26 -0400

    Add some structure and error handling to fetch+parse process.

--- jobs/import-events-database-spreadsheet.js
+++ jobs/import-events-database-spreadsheet.js
@@ -3,16 +3,37 @@ const fetch = require('node-fetch');
 const db = require('../lib/database'); // Has side effect of connecting to database
 // const Event = require('../models/osdi/event');
 
+const columns = [
+
+];
+
 const importEvents = async function () {
   const csvFile = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRk9VzF_6cp_W3zcE_HkBBa5IpiHDLJk9Xx5J1y7OQ_pR_nZMjfuSZYgN4PhlNJNCF9EN-zdjpARtg/pub?gid=393144295&single=true&output=csv';
   console.log(`Loading ${csvFile}`);
 
-  const csv = await fetch(csvFile)
-    .then(response => response.text())
-    .then(body => csvParse(body));
+  const parseOptions = {
+    trim: true,
+    skip_lines_with_error: true
+  };
 
-  console.log(`Found ${csv.length} events`);
+  const rows = await fetch(csvFile)
+    .then(response => response.text())
+    .then(body => csvParse(body, parseOptions));
+
+  colHeadings = rows[0];
+  events = rows.slice(1);
+
+  console.log(`Found ${events.length} events`);
+
+  if (colHeadings.length !== columns.length) {
+    throw(`ERROR: Expected ${columns.length} columns, but found ${colHeadings.length}.`);
+  }
 };
 
-importEvents();
-db.disconnect();
+importEvents()
+  .then(() => db.disconnect())
+  .catch((err) => {
+    db.disconnect();
+    console.error(err);
+    process.exit(1);
+  });
