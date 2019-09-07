bf712697c2d57a752acd0c62a36408539dd29686
Leon Miller-Out <leon@singlebrook.com>
2019-09-07 16:32:20 -0400

    Fix city lookup by coords and return all available city data.

--- routes/handlers/city.js
+++ routes/handlers/city.js
@@ -9,9 +9,8 @@ const get = function (opts, next) {
   Joi.validate(opts.query, OPTS_SCHEMA, function (err, query) {
     if (err) handleError(next, 'validating', err);
     const coordinates = JSON.parse(opts.query.coords);
-    const response = zipcodes.lookup(coordinates[1], coordinates[0]);
-    const city = response ? response.city : undefined;
-    next(null, city);
+    const response = zipcodes.lookupByCoords(coordinates[1], coordinates[0]);
+    next(null, response);
   });
 };
 
